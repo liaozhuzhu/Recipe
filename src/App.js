fe5b66fe7c8efdbe0f5a99d3e48f74533d05f3f6
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-29 12:46:34 -0500

    Added API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 const App = () => {
   const APP_ID = "cda2c799";
   const APP_KEY = "b2405b2b0ae79d4f6e6aeb8290b61842";
+  const req = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Run");
+  });
+
   return (
     <div className="App">
-      <h1>Recipe</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   )
 }
