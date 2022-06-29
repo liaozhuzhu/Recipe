8209d762743b0b3e1e4c4f10e289920db5dd4591
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-28 23:16:19 -0500

    API Key

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = "cda2c799";
+  const APP_KEY = "b2405b2b0ae79d4f6e6aeb8290b61842";
   return (
     <div className="App">
-      
+      <h1>Recipe</h1>
     </div>
-  );
+  )
 }
 
 export default App;
