ddf30765d71e2e8f0beed54074e8651502e28e96
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-29 17:13:11 -0500

    Added Recipe.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, {useEffect, useState} from "react";
+import Recipe from "./components/Recipe";
 import './App.css';
 
 const App = () => {
   const APP_ID = "cda2c799";
   const APP_KEY = "b2405b2b0ae79d4f6e6aeb8290b61842";
 
+  const [recipes, setRecipes] = useState([]);
+
   useEffect(() => {
-    console.log("Run");
-  });
+    getRecipes();
+  }, []);
 
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
   }
   return (
     <div className="App">
@@ -20,6 +23,9 @@ const App = () => {
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe =>(
+        <Recipe/>
+      ))};
     </div>
   )
 }
