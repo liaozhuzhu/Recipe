2e0718d4abe41163daf9885a6370bb15eda1043b
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-29 12:53:32 -0500

    Added Fetch Function

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 const App = () => {
   const APP_ID = "cda2c799";
   const APP_KEY = "b2405b2b0ae79d4f6e6aeb8290b61842";
-  const req = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     console.log("Run");
   });
 
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    console.log(data);
+  }
   return (
     <div className="App">
       <form className="search-form">
